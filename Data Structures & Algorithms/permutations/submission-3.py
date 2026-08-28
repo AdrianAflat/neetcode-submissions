f35b66667902783c33b1592585c7e30e5d16b76b
Adrian Aflatooni <adrianaflatooni@gmail.com>
2026-08-28 15:52:41 -0700

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        sublist = []
+        chosen = [False] * len(nums)
+        def backtrack():
+            if len(sublist) >= len(nums):
+                res.append(sublist.copy())
+                return
+
+            for i in range(len(nums)):
+                if chosen[i] == False:
+                    sublist.append(nums[i])
+                    chosen[i] = True
+                    backtrack()
+
+                    sublist.pop()
+                    chosen[i] = False
+                    
+
+        backtrack()
+        return res
